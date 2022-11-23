be31eab4eee73bb41c602feb9ccdaf40061b68a9
David Chechel <david@rocketfirm.com>
2022-11-23 18:06:35 +0600

    les6 4:34

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tag;
 use Illuminate\Http\Request;
+
 
 class MainController extends Controller
 {
+
     public function index() {
+        // $test= new Tag();
+        // $test->title = 'Я люблю собак';
+        // $test->save();
+        $t = Tag::get
+
         return view('admin.index');
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function tags()
+    {
+        $this->belongsToMany(Tag::class);
+    }
+
+    public function category()
+    {
+        $this->hasOne(Category::class);
+    }
 }
